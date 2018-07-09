42d6e3bff0afa58306c6eb7cbdfe9a19ec87c8d5
Jorge Niedbalski <jorge.niedbalski@linaro.org>
2018-07-09 17:01:29 -0400

    added python client

---
+++ example_client/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	zmq "github.com/pebbe/zmq4"
+)
+func main()() {
+	subscriber, _ := zmq.NewSocket(zmq.SUB)
+	defer subscriber.Close()
+	subscriber.Connect("tcp://localhost:5563")
+	subscriber.SetSubscribe("alerts")
+	for {
+		address, _ := subscriber.Recv(0)
+		content, _ := subscriber.Recv(0)
+		print("[" + string(address) + "] " + string(content) + "\n")
+	}
+}
